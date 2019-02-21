53fb7f3210999cd7585fffe618163e105af08256
kiptetole <jonomi97@gmail.com>
2019-02-21 10:10:22 +0100

    Correccion de errores

--- Ejercicios Practicos/Python/Ejercicio_Tiempo/Tiempo.py
+++ Ejercicios Practicos/Python/Ejercicio_Tiempo/Tiempo.py
@@ -16,13 +16,13 @@ class Tiempo():
         
         contador = 0
         while seg>59:
-            contador = contador + 1
+            contador += 1
             seg = seg - 60
         self.minutos = contador
-        mun = mun + contador
+        mun += contador
         contador = 0
         while mun > 59:
-            contador = contador + 1
+            contador += 1
             seg = seg - 60
         h = h + contador
         while h > 23:
@@ -31,6 +31,7 @@ class Tiempo():
         self.segundos = seg
         self.minutos = mun 
         self.horas = h
+    
     ## Menu de seleccion ##  
     def menu(self):
         print("----Menu Tiempo-----")
@@ -45,20 +46,22 @@ class Tiempo():
     
     ## Suma una hora a la guardada por medio de parametro ##
     def sumar(self, h,mun,seg):
-        
+        ## Se le suman a los segundos guardados los segundos pedidos. ##
+        seg += self.segundos
         contador = 0
-        seg = self.segundos + seg 
         while seg>59:
             contador = contador + 1
-            seg = seg - 60
+            seg -= 60
+        ## Se le suman a los minutos guardados los minutos pedidos y los minutos extras de los segundos. ##
         mun = mun + contador + self.minutos
         contador = 0
         while mun > 59:
             contador = contador + 1
-            mun = mun - 60
+            mun -= 60
+        ## Se le suman a las horas guardados las horas pedidas y las Horas extras de los minutos. ##
         h = h + contador + self.horas
         while h > 23:
-            h = h - 24
+            h -= 24
 
         self.segundos = seg 
         self.minutos = mun
@@ -67,16 +70,19 @@ class Tiempo():
     ## Resta una hora a la guardada por medio de parametro ##
     def restar(self, h, mun, seg):
         
+        ## Se le restan a los segundos guardados los segundos pedidos. ##
+        seg += self.segundos
         contador = 0
-        seg = self.segundos + seg
         while seg<0:
-            contador = contador - 1
-            seg = seg + 60
+            contador -= 1
+            seg += 60
+        ## Se le restan a los minutos guardados los minutos pedidos y los minutos extras de los segundos. ##
         mun = self.minutos + mun + contador
-        while mun<0:
-            contador = contador - 1
-            mun = mun + 60
         contador = 0
+        while mun<0:
+            contador -= 1
+            mun += 60
+        ## Se le restan a las horas guardados las horas pedidas y las Horas extras de los minutos. ##
         h = self.horas + h + contador
         while h<0:
             h = h + 24
